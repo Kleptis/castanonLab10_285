c2676d740f5d39126ec13380dcaf468c29c86614
sean castanon <seacast@umich.edu>
2022-04-14 15:34:38 -0400

    k

--- src/castanonLab10/testSelectionSort.java
+++ src/castanonLab10/testSelectionSort.java
@@ -14,31 +14,20 @@ public class testSelectionSort{
 public void test() {
 testPositive();
 testNegative();
-testMixed();
-testDuplicates();
 }
     public testSelectionSort() {
     }
-    
+
     public void testPositive(){
         testing(8,9,7,10,2);
         /** add tests to check for this unit test **/
     }
-    
+
     public void testNegative(){
         /** Test data contains negative values only **/
     	testing(-8,-9,-5,-10,-2);
     }
-    
-    public void testMixed(){
-        /** Test data contains with both positive, negative and zeros **/
-		testing(-8,9,0,-10,2);//2,7,8,9,10
-    }
-    
-    public void testDuplicates(){
-    	testing(0,0,1,1,1);
-        /** Test data contains duplicates **/
-    }
+
     public void testing(int A, int B, int C, int D, int E) {
     	//int[] Sortedarr = {2,7,8,9,10};
     	int[] arr = {A,B,C,D,E};
